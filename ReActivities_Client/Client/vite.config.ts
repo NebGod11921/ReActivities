bd46b58d1b8fd7d5c9a16dd799ae1c182f4acee4
NebgoD <luandokhacthanh@gmail.com>
2026-03-18 16:13:42 +0700

    Remove define in vite config

--- ReActivities_Client/Client/vite.config.ts
+++ ReActivities_Client/Client/vite.config.ts
@@ -16,10 +16,5 @@ export default defineConfig({
         react(),
 
     ],
-    define: {
-        'import.meta.env.VITE_GITHUB_CLIENT_ID':
-            JSON.stringify(process.env.VITE_GITHUB_CLIENT_ID),
-        'import.meta.env.VITE_REDIRECT_URL':
-            JSON.stringify(process.env.VITE_REDIRECT_URL),
-    }
+
 })
