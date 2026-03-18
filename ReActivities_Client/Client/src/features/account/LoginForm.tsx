086b1e1467263c646204108dd5cb5965494c5ac7
NebgoD <luandokhacthanh@gmail.com>
2026-03-18 17:04:53 +0700

    Update version

--- ReActivities_Client/Client/src/features/account/LoginForm.tsx
+++ ReActivities_Client/Client/src/features/account/LoginForm.tsx
@@ -35,11 +35,9 @@ export default function LoginForm() {
 
     const loginWithGitHub = () => {
         const clientId = import.meta.env.VITE_GITHUB_CLIENT_ID;
-        const redirectUrl = import.meta.env.VITE_REDIRECT_URL;
+        const redirectUri = import.meta.env.VITE_REDIRECT_URI;
 
-        console.log("redirectUrl", redirectUrl);
-        console.log("clientId", clientId);
-        window.location.href = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_Uri=${redirectUrl}&scope=read:user user:email`;
+        window.location.href = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=read:user user:email`;
     }
 
 
